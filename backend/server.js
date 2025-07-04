153aae6b06f92675c5531d9cf5d0c610b6bca720
dileepchandaka9949 <dileepchandaka123@gmail.com>
2025-07-04 22:08:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,20 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const apiRoutes = require("./routes/api");
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Welcome to DileepGPT Backend!');
+// Link API routes
+app.use("/api", apiRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to DileepGPT Backend!");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
