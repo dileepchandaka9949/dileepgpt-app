50ebe1f037eedc5f7b007236a9359958162ea4b4
dileepchandaka9949 <dileepchandaka123@gmail.com>
2025-07-04 21:46:02 +0530

    Create App.js

---
+++ frontend/frontend/src/frontend/src/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Welcome to DileepGPT</h1>
+      <p>Powered by Dileep Chandaka 🚀</p>
+    </div>
+  );
+}
+
+export default App;
